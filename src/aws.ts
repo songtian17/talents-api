2e470a64d4bedf258b6027161a696a14bbff503b
songtian17 <liust.17@ichat.sp.edu.sg>
2020-02-17 12:17:15 +0800

    Fix async method

--- src/aws.ts
+++ src/aws.ts
@@ -5,22 +5,24 @@ AWS.config.loadFromPath("./awsconfig.json");
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 export function getAccountIdFromToken(token: string) {
-  return docClient.get(
-    {
-      TableName: "sessions",
-      Key: { token: token }
-    },
-    (err, data) => {
-      if (err) {
-        console.error(
-          "Unable to read item. Error JSON:",
-          JSON.stringify(err, null, 2)
-        );
-        return undefined;
-      }
+  return new Promise(function(resolve, reject) {
+    docClient.get(
+      {
+        TableName: "sessions",
+        Key: { token: token }
+      },
+      function(err, data) {
+        if (err) {
+          console.error(
+            "Unable to read item. Error JSON:",
+            JSON.stringify(err, null, 2)
+          );
+          reject(err);
+        }
 
-      console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-      return data.accountId;
-    }
-  );
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        resolve(JSON.parse(JSON.stringify(data)));
+      }
+    );
+  });
 }
--- src/middlewares/checkSession.ts
+++ src/middlewares/checkSession.ts
@@ -1,14 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import { getAccountIdFromToken } from "../aws";
 
-export const isAuthenticated = (
+export const isAuthenticated = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   let authHeader = req.header("Authorization");
   let token = authHeader.split(" ")[1];
-  let accountId = getAccountIdFromToken(token);
+  let accountId = await getAccountIdFromToken(token);
   console.log("accountId:", accountId);
   if (!accountId) {
     res.sendStatus(401);
